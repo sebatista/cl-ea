51abde945f2959f2fb25f6b1a3f5bc59a5d413cd
Jorge Obiols <jorge.obiols@gmail.com>
2021-01-27 13:38:17 -0300

    [FIX] excepcion en imprimir remito

--- ea_aeroo_stock/models/res_partner.py
+++ ea_aeroo_stock/models/res_partner.py
@@ -4,7 +4,8 @@ class ResPartner(models.Model):
     _inherit = "res.partner"
 
 
-    delivery_address_id = fields.Char(
+    delivery_address_id = fields.Many2one(
+        'res.partner',
         compute="_compute_delivery_address"
     )
 
@@ -12,5 +13,5 @@ class ResPartner(models.Model):
     def _compute_delivery_address(self):
 
         for rec in self:
-            delivery = rec.child_ids.filtered(lambda r: r.type == "address")
-            rec.delivery_address_id = delivery[0] if delivery else rec
+            delivery = rec.child_ids.filtered(lambda r: r.type == "delivery")
+            rec.delivery_address_id = delivery[0] if delivery else rec.street
--- ea_aeroo_stock/readme/DESCRIPTION.rst
+++ ea_aeroo_stock/readme/DESCRIPTION.rst
@@ -1,1 +1,7 @@
 Este modulo modifica los datos impresos en el remito original de aeroo.
+
+Se puede cambiar la direccion de envio del cliente agregando
+en la solapa Contactos y direcciones un contacto de tipo **Dirección de envío**
+
+Tener en cuenta que debe haber una sola direccion de envio, ya que si
+hay varias se usara una, que no es necesariamente la primera que hay.
