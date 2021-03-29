56c08937d7018df0226156edb108a1ecd7042eee
Jorge Obiols <jorge.obiols@gmail.com>
2021-03-29 13:21:58 -0300

    [FIX] repo branch

--- ea_default/__manifest__.py
+++ ea_default/__manifest__.py
@@ -91,7 +91,7 @@
         'https://github.com/itpp-labs/mail-addons.git itpp-mail-addons',
         'https://github.com/itpp-labs/misc-addons.git itpp-misc-addons',
         'https://github.com/itpp-labs/website-addons.git itpp-misc-addons',
-        'https://github.com/sebatista/EA_theme_laze.git seba-theme-laze'
+        'https://github.com/sebatista/EA_theme_laze.git seba-theme-laze -b main'
     ],
 
     # list of images to use in the form 'name image-url'
