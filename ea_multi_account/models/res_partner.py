ac1f1cce06e3bb53ef125f115d327dd9864550c6
Jorge Obiols <jorge.obiols@gmail.com>
2021-06-21 20:46:23 -0300

    [FIX] #1 Multi account - Crear Cliente desde usuario comercial
    Se agrego un parametro para cargar las cuentas por defecto
    
    receivable_account_x	X1.1.02.00.010
    payable_account_x	X2.1.01.01.010

--- ea_multi_account/models/res_partner.py
+++ ea_multi_account/models/res_partner.py
@@ -7,11 +7,23 @@ class ResPartner(models.Model):
         'account.account',
         domain="[('internal_type', '=', 'receivable'),('deprecated', '=', False)]",
         required=True,
-        string="Cuenta a cobrar X"
+        string="Cuenta a cobrar X",
+        default=lambda self: self.default_receivable_x()
     )
     property_account_payable_x_id = fields.Many2one(
         'account.account',
         domain="[('internal_type', '=', 'payable'),('deprecated', '=', False)]",
         required=True,
-        string="Cuenta a pagar X"
+        string="Cuenta a pagar X",
+        default=lambda self: self.default_payable_x()
     )
+
+    def default_receivable_x(self):
+        config = self.env["ir.config_parameter"].sudo()
+        code = config.get_param('receivable_account_x')
+        return self.env['account.account'].search([('code', '=', code)])
+
+    def default_payable_x(self):
+        config = self.env["ir.config_parameter"].sudo()
+        code = config.get_param('payable_account_x')
+        return self.env['account.account'].search([('code', '=', code)])
