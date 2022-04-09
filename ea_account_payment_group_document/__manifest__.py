aa507f9328538721d377614f13c7ed1403b6b20a
matias <mmoriena@quilsoft.com>
2022-04-09 14:10:57 -0300

    [ADD] VTA_009 – Recibo de Cobranza [ADD] VAT_010 - Talonarios de Recibo según Local

---
+++ ea_account_payment_group_document/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "EA Payment Groups with Accounting Documents Extended",
+    "version": "11.0.0.0.1",
+    "author": "jeo Software",
+    "license": "AGPL-3",
+    "category": "Accounting",
+    "depends": [
+        "account_payment_group_document",
+    ],
+    "data": [
+        "view/account_payment_group_view.xml",
+        "view/account_payment_receiptbook_view.xml",
+        "data/payment_report.xml",
+    ],
+    "demo": [],
+    "images": [],
+    "installable": True,
+}
