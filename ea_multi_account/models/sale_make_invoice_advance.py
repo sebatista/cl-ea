2c3b70733beb5471febeedc7d55037d449aa0118
Jorge Obiols <jorge.obiols@gmail.com>
2022-06-11 19:14:18 -0300

    [FIX] cuentas X verificadas

--- ea_multi_account/models/sale_make_invoice_advance.py
+++ ea_multi_account/models/sale_make_invoice_advance.py
@@ -11,18 +11,22 @@ class SaleAdvancePaymentInv(models.TransientModel):
     @api.multi
     def _create_invoice(self, order, so_line, amount):
         """sobreescribimos este metodo para pasarle a la factura el tipo de venta
-        Aca entra solo cuando hacen un down payment
+        Aca entra solo cuando hacen un down payment solo facturas de compra o sea
+        out_invoice
         """
         ret = super(SaleAdvancePaymentInv, self)._create_invoice(order, so_line, amount)
         ret.sale_type_id = order.type_id
+        ret.journal_id = order.type_id.journal_id.id
 
         product = self.product_id
         xxx = order.type_id.account_x
         accounts = product.product_tmpl_id._get_product_accounts()
         if xxx:
             account = accounts["income_x"]
+            ret.account_id = ret.partner_id.property_account_receivable_x_id
         else:
             account = accounts["income"]
+            ret.account_id = ret.partner_id.property_account_receivable_id
 
         if not account:
             raise UserError(
