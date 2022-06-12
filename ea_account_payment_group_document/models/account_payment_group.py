8d972545c7fe51641a3c321897cde6226a4785c3
matias <mmoriena@quilsoft.com>
2022-06-12 20:55:59 -0300

    [FIX] se arreglo el problema de guadar el recibo issue #2

--- ea_account_payment_group_document/models/account_payment_group.py
+++ ea_account_payment_group_document/models/account_payment_group.py
@@ -7,9 +7,43 @@ class AccountPaymentGroup(models.Model):
 
     operating_unit_ids = fields.Many2many(
         "operating.unit",
-        "operating_unit_users_rel",
-        "user_id",
-        "poid",
-        "Operating Units",
+        compute="compute_unit_ids",
         default=lambda self: self.env.user.operating_unit_ids.ids,
     )
+
+    def compute_unit_ids(self):
+
+        ou_ids = self.env.user.operating_unit_ids
+
+        for record in self:
+            if ou_ids:
+                record.operating_unit_ids = ou_ids.ids
+            else:
+                record.operating_unit_ids = None
+
+    @api.multi
+    def _get_receiptbook(self):
+
+        self.ensure_one()
+        partner_type = self.partner_type or self._context.get(
+            "partner_type", self._context.get("default_partner_type", False)
+        )
+        ou_ids = self.env.user.operating_unit_ids
+        if ou_ids:
+            receiptbook = self.env["account.payment.receiptbook"].search(
+                [
+                    ("partner_type", "=", partner_type),
+                    ("company_id", "=", self.company_id.id),
+                    ("operating_unit_id", "in", ou_ids.ids),
+                ],
+                limit=1,
+            )
+        else:
+            receiptbook = self.env["account.payment.receiptbook"].search(
+                [
+                    ("partner_type", "=", partner_type),
+                    ("company_id", "=", self.company_id.id),
+                ],
+                limit=1,
+            )
+        return receiptbook
